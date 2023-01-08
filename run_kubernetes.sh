4ac8f17bdc23d45a0ae4be0366b919ab9305dcb4
Adeleye <adewale.adeleye@flutterwavego.com>
2023-01-08 20:07:06 +0100

    kubernetes step done

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -8,11 +8,11 @@ dockerpath=python-prediction
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl create deploy python-prediction --image=adelee2/$dockerpath
+kubectl create deploy pythonprediction --image=adelee2/$dockerpath
 
 # Step 3:
 # List kubernetes pods
 kubectl get pods
 # Step 4:
 # Forward the container port to a host
-# kubectl port-forward pod/python-prediction-c645bcd-ck62w --address 0.0.0.0 80:80
+kubectl port-forward pod/pythonprediction-679b444c56-gb9cw --address 0.0.0.0 80:80
